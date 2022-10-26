60a7d29deec638a94672ff4458bc132e524b75c1
linux EGAMER <evandrogomes542@gmail.com>
2022-10-26 18:17:55 -0300

    começei a realizar a seção dos meu projetos

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 
 
 function work() {
+projetos();
+myProjectson()
+
+}
+
+function projetos() {
 
     
     gsap.to(".lua", {x:-900, duration:2, rotate:280, ease: "back"});
@@ -17,8 +23,13 @@ function work() {
     gsap.to(".divsecond", {opacity:1, duration:3, ease: "back", });
     gsap.to("nav", {x:200, y:60, duration:3, ease: "back"});
 }
+function homee() {
+    myProjectsout();
+    home()
+}
 
 function home() {
+
     gsap.to(".lua", {x:0, duration:2, rotate:280, ease: "back"});
     gsap.to(".lua1", {x:0, duration:4, rotate:50, ease: "back"});
     gsap.to(".lua2", {x:0, y:-50, duration:3, rotate:50, ease: "back"});
@@ -42,8 +53,59 @@ function waves() {
     gsap.to(".lua4", {x:70,y:40 , duration:3, rotate:100, ease: "power1", yoyo:true});
     
     gsap.to(".eclipse", {x:0, duration:3, rotate:50, ease: "back"});
-    
+}
+
+ function myProjectson() {
+   gsap.to(".legalite", {x:0, y:0, duration:1, ease:"power1", yoyo: true})
+}
+function myProjectsout() {
+    gsap.to(".legalite", {x:800, y:700})
+
+}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var doctorrclick = document.querySelector('.projetos .doctor')
+
+
+function doctorclick() {
+    if (doctorrclick.classList.contains('doctorclick')) { 
+    doctorrclick.classList.remove('doctorclick')}
+    else {
+        doctorrclick.classList.add('doctorclick')
+    }
+}
+function doctorhover() {
+    gsap.to(".doctor", {x:-500,y:200 , duration:1, ease: "power1", yoyo:true})
+}
+var llegaliteclick = document.querySelector('.projetos .legalite')
+
+
+function legaliteclick() {
+    if (llegaliteclick.classList.contains('legaliteclick')) { 
+    llegaliteclick.classList.remove('legaliteclick')}
+    else {
+        llegaliteclick.classList.add('legaliteclick')
+    }
+}
+function legalitehover() {
+    if (llegaliteclick.classList.contains('legalitehover')) { 
+    llegaliteclick.classList.remove('legalitehover')}
+    else {
+        llegaliteclick.classList.add('legalitehover')
+    }
 }
