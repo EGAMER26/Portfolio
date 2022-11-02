3f0b18a29ac55f5f2c17b7b43251187daf7f03e4
linux EGAMER <evandrogomes542@gmail.com>
2022-11-02 19:34:05 -0300

    notebook feito junto com as animações, faltar arrumar um bug do bitão celular

---
+++ assets/pure-css-notebook/README.md
@@ -0,0 +1,7 @@
+# Pure CSS Notebook
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/skimberk1/pen/neRoGv](https://codepen.io/skimberk1/pen/neRoGv).
+
+It is also completely scalable as it's size depends entirely on it's font size. 
+
+Inspired by this Dribble shot: http://dribbble.com/shots/1148286-Simple-Notebook
--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
 
-
+let backblack = document.querySelector('body')
 
 
 function projetos() {
 
     
-    gsap.to(".lua", {x:-900, duration:2, rotate:280, ease: "back"});
-    gsap.to(".lua1", {x:-900, duration:4, rotate:50, ease: "back"});
-    gsap.to(".lua2", {x:-900, y:-50, duration:3, rotate:50, ease: "back"});
-    gsap.to(".lua3", {x:-900, y:200, duration:3, rotate:50, ease: "back"});
-    gsap.to(".lua4", {x:-900, duration:3, rotate:50, ease: "back"});
+    gsap.to(".lua", {x:-900, duration:2, rotate:380, ease: "back"});
+    gsap.to(".lua1", {x:-900, duration:4, rotate:250, ease: "back"});
+    gsap.to(".lua2", {x:-900, y:-50, duration:3, rotate:150, ease: "back"});
+    gsap.to(".lua3", {x:-900, y:200, duration:3, rotate:350, ease: "back"});
+    gsap.to(".lua4", {x:-900, duration:3, rotate:450, ease: "back"});
     
     gsap.to(".eclipse", {x:-970, duration:3, rotate:50, ease: "back"});
     
@@ -18,10 +18,14 @@ function projetos() {
     
     gsap.to(".divsecond", {opacity:1, duration:3, ease: "back", });
     gsap.to("nav", {x:200, y:60, duration:3, ease: "back"});
+    
+    gsap.to(".allMenu", {y:-660, duration:2, ease: "back"});
 }
 function homee() {
     myProjectsout();
-    home()
+    home();
+    backbody()
+
 }
 
 function home() {
@@ -36,10 +40,44 @@ function home() {
     
     gsap.fromTo(".content", {opacity:0, x:-1000}, {opacity:1, x:0, duration:2 ,ease: "back",force3d:true});
     
-    gsap.to(".divsecond", {opacity:0, duration:1, ease: "back", });
+    gsap.to(".divsecond", {opacity:0, duration:1, ease: "back"});
     gsap.to("nav", {x:0, y:0, duration:3, ease: "back"});
+
+    gsap.to(".allMenu", {y:-860, duration:1, ease: "back"});
+    gsap.to(".notebook", {y:1000, duration:3, ease: "back"});
     
 }
+
+let allblack = document.querySelector('body')
+
+function backbody(item, index) {
+    if(backblack.classList.contains('bodyback')) {
+        backblack.classList.remove('bodyback')
+    } else {
+        backblack.classList.add('bodyback')
+    } // function clickItem(item, index) {
+   
+
+    //     backblack.style.removeProperty("--timeOut");
+        
+    //     if (activeItem == item) return;
+        
+    //     if (activeItem) {
+    //         activeItem.classList.remove("active");
+    //     }
+    
+        
+    //     item.classList.add("active");
+    //     body.style.backgroundColor = blackback[index];
+    //     activeItem = item;
+    //     offsetMenuBorder(activeItem, menuBorder);
+        
+        
+    }
+    function outbody(item, index) {
+        if(backblack.classList.contains('bodyback')) {
+            backblack.classList.remove('bodyback')
+        }}
 
 function waves() {
     gsap.fromto(".lua", {x:30,y:20 , duration:3, rotate:100, ease: "power1", yoyo:true});
@@ -52,7 +90,8 @@ function waves() {
 }
 function work() {
     projetos();
-    myProjectson()
+    pcButton();
+    outbody()
     
     }
 
@@ -66,7 +105,7 @@ function myProjectson() {
     gsap.to(".iphone5", {x:1402, y:-1050, duration:3, ease: "elastic.out(1,1, 0.6)"})
  }
  function myProjectsout() {
-     gsap.to(".iphone5", {y:700, duration:3, ease:"back"})
+     gsap.to(".iphone5", {y:-2000, duration:3, ease:"back"})
  
  }
 function iiframeclick() {
@@ -96,5 +135,82 @@ function doctorclick() {
 }
 function doctorhover() {
     gsap.to(".doctor", {x:-500,y:200 , duration:1, ease: "power1", yoyo:true})
+}
+
+
+
+// ------------------------------------------------------------------------------------------------
+// Designed by:  Mauricio Bucardo
+// Original image:
+// https://dribbble.com/shots/5619509-Animated-Tab-Bar
+
+"use strict"; 
+
+const body = document.body;
+const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
+const menu = body.querySelector(".menu");
+const menuItems = menu.querySelectorAll(".menu__item");
+const menuBorder = menu.querySelector(".menu__border");
+let activeItem = menu.querySelector(".active");
+const blackback = ["#000"];
+
+
+function clickItem(item, index) {
+
+    menu.style.removeProperty("--timeOut");
+    
+    if (activeItem == item) return;
+    
+    if (activeItem) {
+        activeItem.classList.remove("active");
+    }
+
+    
+    item.classList.add("active");
+    body.style.backgroundColor = bgColorsBody[index];
+    activeItem = item;
+    offsetMenuBorder(activeItem, menuBorder);
+    
+    
+}
+
+function offsetMenuBorder(element, menuBorder) {
+
+    const offsetActiveItem = element.getBoundingClientRect();
+    const left = Math.floor(offsetActiveItem.left - menu.offsetLeft - (menuBorder.offsetWidth  - offsetActiveItem.width) / 2) +  "px";
+    menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
+
+}
+
+offsetMenuBorder(activeItem, menuBorder);
+
+menuItems.forEach((item, index) => {
+
+    item.addEventListener("click", () => clickItem(item, index));
+    
+})
+
+window.addEventListener("resize", () => {
+    offsetMenuBorder(activeItem, menuBorder);
+    menu.style.setProperty("--timeOut", "none");
+});
+
+//----------------------------------MENU---------------------------------
+
+function pcButton() {
+    gsap.to(".notebook", {y:-1250, duration:2, ease:"back"});
+    gsap.to(".iphone5", {y:-100, duration:2, ease:"back"})
+    
+
+    // gsap.to(".lua", {x:-900, duration:2, rotate:380, ease: "back"});
+
+}
+function iphoneButton() {
+    gsap.to(".notebook", {y:500, duration:2, ease:"back"});
+    gsap.to(".iphone5", {y:1320, duration:2, ease:"back"})
+    
+
+    // gsap.to(".lua", {x:-900, duration:2, rotate:380, ease: "back"});
+
 }
 
